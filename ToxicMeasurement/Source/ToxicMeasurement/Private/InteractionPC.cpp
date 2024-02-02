14263acf60cd676041e72907391d9e6823d24c47
Professor0Ice <matt_bmw@mail.ru>
2024-02-03 00:09:07 +0500

    Update 0.7 Reload Tutorial

---
+++ ToxicMeasurement/Source/ToxicMeasurement/Private/InteractionPC.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "InteractionPC.h"
+
+InteractionPC::InteractionPC()
+{
+}
+
+InteractionPC::~InteractionPC()
+{
+}
+
+void AreturnFuncParametresCharacter::testFunctionAB(int a, int b, int& result)
+{
+	result = a + b;
+}
---
+++ ToxicMeasurement/Source/ToxicMeasurement/Public/InteractionPC.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class TOXICMEASUREMENT_API InteractionPC
+{
+public:
+	UFUNCTION(BlueprintCallable)
+		void testFunctionAB(int a, int b, int& result);
+
+};
